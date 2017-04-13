618a473b7aaff3a5974e76b46ab6a9b13d951259
Toof_Appsia <toanf9dn@gmail.com>
2017-04-13 20:22:00 +0700

    Update podspec

--- Example/RealmObjc/ViewController.swift
+++ Example/RealmObjc/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import RealmObjc
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
--- RealmObjc.podspec
+++ RealmObjc.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'RealmObjc'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'RealmObjc which help you to query faster in Realm.'
 
 # This description is used to generate tags and improve search results.
